44c8e6c1d11782a7f0d55725cd3426d6702bea9d
Sstomi <40359766+Sstomi@users.noreply.github.com>
2024-11-23 11:42:25 +0900

    fix GA add

--- app/third-parties/google-analytics.js
+++ app/third-parties/google-analytics.js
@@ -1,28 +1,26 @@
+'use client'; 
+import Script from 'next/script' 
+
 export default function GoogleAnalytics() {
-    // 環境変数から測定IDを取得
-      const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
-    
-      return (
-        <>
-          {/* Google Analytics の読み込み */}
-          <script
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-            strategy="afterInteractive"
-          />
-    
-          {/* 初期化コード */}
-          <script
-            id="google-analytics"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${GA_ID}');
-              `,
-            }}
-          />
-        </>
-      )
-    }
+    const GA_ID = process.env.NEXT_PUBLIC_GA_ID
+  
+    return (
+      <>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+        >
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}');
+          `}
+        </Script>
+      </>
+    )
+  }
